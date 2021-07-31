65f2988e29fb01cddd656c5d7695b4f8a50e55a0
wlm2012 <541253785@qq.com>
2021-07-31 17:48:50 +0800

    ebook

--- moocWiki/src/main/java/com/study/moocwiki/mapper/ebook.java
+++ moocWiki/src/main/java/com/study/moocwiki/mapper/ebook.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -19,6 +20,7 @@ public class ebook {
 
     @Id
     private long id;
+    @Column(length = 50)
     private String name;
 
 }
