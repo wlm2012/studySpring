6d22632a1c14438926989988e53aad1e47ab3944
wlm2012 <541253785@qq.com>
2021-07-31 17:44:32 +0800

    ebook

---
+++ moocWiki/src/main/java/com/study/moocwiki/mapper/Book.java
@@ -0,0 +1,24 @@
+package com.study.moocwiki.mapper;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ebook")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class Book {
+
+    @Id
+    private long id;
+    private String name;
+
+}
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -7,11 +7,18 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import {defineComponent} from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue';
+import axios from "axios"; // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
+  setup() {
+    console.log("setup")
+    axios.get("").then((response) => {
+      console.log(response)
+    })
+  },
   components: {
     HelloWorld,
   },
