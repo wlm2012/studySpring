7173506489c25db2bc775e152fa2b817a2d7f834
wlm2012 <541253785@qq.com>
2021-07-31 17:59:21 +0800

    ebook

--- moocWiki/src/main/java/com/study/moocwiki/mapper/ebook.java
+++ moocWiki/src/main/java/com/study/moocwiki/mapper/ebook.java
@@ -11,7 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "ebook")
+@Table(name = "ebook" , catalog = "电子书")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
@@ -19,8 +19,31 @@ import javax.persistence.Table;
 public class ebook {
 
     @Id
+    @Column(nullable = false)
     private long id;
-    @Column(length = 50)
+
+    @Column(length = 50, columnDefinition = "名称")
     private String name;
+
+    @Column(columnDefinition = "分类1")
+    private long category1Id;
+
+    @Column(columnDefinition = "分类2")
+    private long category2Id;
+
+    @Column(length = 200, columnDefinition = "描述")
+    private String description;
+
+    @Column(length = 200, columnDefinition = "封面")
+    private String cover;
+
+    @Column(columnDefinition = "文档数")
+    private int docCount;
+
+    @Column(columnDefinition = "阅读数")
+    private int viewCount;
+
+    @Column(columnDefinition = "点赞数")
+    private int voteCount;
 
 }
