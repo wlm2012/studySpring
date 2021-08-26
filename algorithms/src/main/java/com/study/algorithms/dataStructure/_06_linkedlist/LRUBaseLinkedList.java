a758c7598f8d70df97c9e29f92a55d057e25b02d
wlm2012 <541253785@qq.com>
2021-08-26 20:21:54 +0800

    list

---
+++ algorithms/src/main/java/com/study/algorithms/dataStructure/_06_linkedlist/LRUBaseLinkedList.java
@@ -0,0 +1,28 @@
+package com.study.algorithms.dataStructure._06_linkedlist;
+
+public class LRUBaseLinkedList<T> {
+
+    /**
+     * 默认链表容量
+     */
+    private final static Integer DEFAULT_CAPACITY = 10;
+
+    /**
+     * 头结点
+     */
+    private SNode<T> headNode;
+
+    /**
+     * 链表长度
+     */
+    private Integer length;
+
+    /**
+     * 链表容量
+     */
+    private Integer capacity;
+
+
+    
+
+}
---
+++ algorithms/src/main/java/com/study/algorithms/dataStructure/_06_linkedlist/SNode.java
@@ -0,0 +1,37 @@
+package com.study.algorithms.dataStructure._06_linkedlist;
+
+public class SNode<T> {
+
+    private T element;
+
+    private SNode<T> next;
+
+    public SNode(T element) {
+        this.element = element;
+    }
+
+    public SNode(T element, SNode<T> next) {
+        this.element = element;
+        this.next = next;
+    }
+
+    public SNode() {
+        this.next = null;
+    }
+
+    public T getElement() {
+        return element;
+    }
+
+    public void setElement(T element) {
+        this.element = element;
+    }
+
+    public SNode<T> getNext() {
+        return next;
+    }
+
+    public void setNext(SNode<T> next) {
+        this.next = next;
+    }
+}
