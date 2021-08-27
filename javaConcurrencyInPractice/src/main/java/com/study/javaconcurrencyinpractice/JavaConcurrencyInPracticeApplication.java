fac4a079dabf82a5170b417d151b3d8eb20522e7
wlm2012 <541253785@qq.com>
2021-08-27 17:23:26 +0800

    Java Concurrency in Practice

---
+++ javaConcurrencyInPractice/src/main/java/com/study/javaconcurrencyinpractice/JavaConcurrencyInPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.study.javaconcurrencyinpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConcurrencyInPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaConcurrencyInPracticeApplication.class, args);
+    }
+
+}
---
+++ javaConcurrencyInPractice/src/test/java/com/study/javaconcurrencyinpractice/JavaConcurrencyInPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.javaconcurrencyinpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaConcurrencyInPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
