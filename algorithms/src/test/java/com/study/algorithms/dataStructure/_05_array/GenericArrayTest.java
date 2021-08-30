099bfeedc180c6d329a6a7fa75669c5b99c10b5d
wlm2012 <541253785@qq.com>
2021-08-30 20:24:21 +0800

    _05_array

---
+++ algorithms/src/test/java/com/study/algorithms/dataStructure/_05_array/GenericArrayTest.java
@@ -0,0 +1,15 @@
+package com.study.algorithms.dataStructure._05_array;
+
+import org.junit.jupiter.api.Test;
+
+public class GenericArrayTest {
+
+    @Test
+    public void arrayTest() {
+        GenericArray<Integer> array = new GenericArray<>();
+        System.out.println(array.get(6));
+        System.out.println(array.get(996));
+    }
+
+
+}
