a3710827aea2fa34f14d724bbc122af280e6a3fb
wlm2012 <541253785@qq.com>
2021-09-04 17:18:30 +0800

    testReturn

--- jpa/src/test/java/com/spring/jpa/repository/UserRepositoryTest.java
+++ jpa/src/test/java/com/spring/jpa/repository/UserRepositoryTest.java
@@ -1,11 +1,11 @@
 package com.spring.jpa.repository;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.spring.jpa.DO.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.data.util.Streamable;
 
 import javax.annotation.Resource;
@@ -46,5 +46,32 @@ public class UserRepositoryTest {
         Streamable<User> ageStream = userRepository.findAll(PageRequest.of(0, 2, Sort.by("name")))
                 .and(User.builder().age(11).build());
         ageStream.forEach(System.out::println);
+    }
+
+
+    @Test
+    public void testReturn() {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        System.out.println("Streamable---------------------");
+        Streamable<User> ageStream = userRepository.findAll(PageRequest.of(0, 2, Sort.by("name")))
+                .and(User.builder().age(11).build());
+        ageStream.forEach(System.out::println);
+
+        System.out.println("Page---------------------");
+        Page<User> page = userRepository.findAll(PageRequest.of(0, 2, Sort.by("name")));
+        System.out.println(page);
+
+        System.out.println("Slice---------------------");
+        Slice<User> userSlice = userRepository.findByAge(11, PageRequest.of(0, 2, Sort.by("name")));
+        System.out.println(userSlice);
+
+        System.out.println("Slice---------------------");
+        Example<User> example = Example.of(User.builder().age(11).build());
+        List<User> list = userRepository.findAll(example, Sort.by("n"));
+        System.out.println(list);
+
+
     }
 }
