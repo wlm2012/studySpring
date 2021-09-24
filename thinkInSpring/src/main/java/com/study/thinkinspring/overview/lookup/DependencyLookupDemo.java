f48b52226fee0c97f081df20df3d6020357d9ddd
wlm2012 <541253785@qq.com>
2021-09-24 20:26:00 +0800

    DependencyLookupDemo

--- thinkInSpring/src/main/java/com/study/thinkinspring/overview/lookup/DependencyLookupDemo.java
+++ thinkInSpring/src/main/java/com/study/thinkinspring/overview/lookup/DependencyLookupDemo.java
@@ -2,7 +2,11 @@ package com.study.thinkinspring.overview.lookup;
 
 import com.study.thinkinspring.overview.domain.User;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ListableBeanFactory;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Map;
 
 
 public class DependencyLookupDemo {
@@ -10,11 +14,34 @@ public class DependencyLookupDemo {
     public static void main(String[] args) {
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("META-INF/dependency-lookup-context.xml");
         lookupInRealTime(beanFactory);
+        lookupInLazy(beanFactory);
+
+        lookupByType(beanFactory);
+
+        lookupCollectionByType(beanFactory);
+    }
+
+    private static void lookupCollectionByType(BeanFactory beanFactory) {
+        if (beanFactory instanceof ListableBeanFactory listableBeanFactory) {
+            Map<String, User> beansOfType = listableBeanFactory.getBeansOfType(User.class);
+            System.out.println("lookupCollectionByType" + beansOfType);
+        }
+    }
+
+    private static void lookupByType(BeanFactory beanFactory) {
+        User user = beanFactory.getBean(User.class);
+        System.out.println("lookupByType" + user);
     }
 
 
-    public static void lookupInRealTime(BeanFactory beanFactory) {
+    private static void lookupInRealTime(BeanFactory beanFactory) {
         User user = (User) beanFactory.getBean("user");
-        System.out.println(user);
+        System.out.println("RealTime" + user);
+    }
+
+    private static void lookupInLazy(BeanFactory beanFactory) {
+        ObjectFactory<User> objectFactory = (ObjectFactory<User>) beanFactory.getBean("objectFactory");
+        User user = objectFactory.getObject();
+        System.out.println("lazy " + user);
     }
 }
