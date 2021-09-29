439def35a1655f5bb849bd99306c14018fb0846b
wlm2012 <541253785@qq.com>
2021-09-29 17:36:38 +0800

    UserRepository

--- jpa/src/main/java/com/spring/jpa/repository/UserRepository.java
+++ jpa/src/main/java/com/spring/jpa/repository/UserRepository.java
@@ -9,13 +9,11 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.LockModeType;
 import javax.transaction.Transactional;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
