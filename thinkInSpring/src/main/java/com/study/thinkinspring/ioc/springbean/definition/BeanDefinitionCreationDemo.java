0f6b1e5eb23fab78946b12d76b1df7ffb3f37cee
wlm2012 <541253785@qq.com>
2021-10-01 11:56:26 +0800

    UserRepository

--- thinkInSpring/src/main/java/com/study/thinkinspring/ioc/springbean/definition/BeanDefinitionCreationDemo.java
+++ thinkInSpring/src/main/java/com/study/thinkinspring/ioc/springbean/definition/BeanDefinitionCreationDemo.java
@@ -1,8 +1,34 @@
 package com.study.thinkinspring.ioc.springbean.definition;
 
+import com.study.thinkinspring.ioc.overview.domain.User;
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
+
 public class BeanDefinitionCreationDemo {
 
     public static void main(String[] args) {
+
+        // 1.通过 BeanDefinitionBuilder 构建
+        BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(User.class);
+        beanDefinitionBuilder
+                .addPropertyValue("id", 1)
+                .addPropertyValue("name", "小马哥");
+        // 获取 BeanDefinition 实例
+        BeanDefinition beanDefinition = beanDefinitionBuilder.getBeanDefinition();
+
+        // 2. 通过 AbstractBeanDefinition 以及派生类
+        GenericBeanDefinition genericBeanDefinition = new GenericBeanDefinition();
+        // 设置 Bean 类型
+        genericBeanDefinition.setBeanClass(User.class);
+        // 通过 MutablePropertyValues 批量操作属性
+        MutablePropertyValues mutablePropertyValues = new MutablePropertyValues();
+        mutablePropertyValues
+                .add("id", 1)
+                .add("name", "小马哥");
+        genericBeanDefinition.setPropertyValues(mutablePropertyValues);
 
     }
 }
