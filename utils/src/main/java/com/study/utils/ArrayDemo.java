2794cfb49b41f2f99e5fe9ce20f09f4ab1f2165c
wlm2012 <541253785@qq.com>
2021-10-04 20:44:20 +0800

    ArrayDemo

---
+++ utils/src/main/java/com/study/utils/ArrayDemo.java
@@ -0,0 +1,8 @@
+package com.study.utils;
+
+public class ArrayDemo {
+
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE + 1 == Integer.MIN_VALUE);
+    }
+}
