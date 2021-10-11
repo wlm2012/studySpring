189ba34fffc5dc8bcfd814ef4caeac80c10ff263
wlm2012 <541253785@qq.com>
2021-10-11 17:46:13 +0800

    jpa

--- jpa/src/test/java/com/spring/jpa/repository/OrderRepositoryTest.java
+++ jpa/src/test/java/com/spring/jpa/repository/OrderRepositoryTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
