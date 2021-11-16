78470f25aed9cb88f94a2e4ae9ca596a0720d253
wlm2012 <541253785@qq.com>
2021-11-16 16:27:24 +0800

    taskExecutor

--- jpa/src/main/java/com/spring/jpa/config/TaskExecutorConfig.java
+++ jpa/src/main/java/com/spring/jpa/config/TaskExecutorConfig.java
@@ -2,7 +2,6 @@ package com.spring.jpa.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.TaskDecorator;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
