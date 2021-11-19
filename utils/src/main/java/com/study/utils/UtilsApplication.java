77f126f8d8f0d94970ecc043fdf9a63afc094f24
wlm2012 <541253785@qq.com>
2021-11-19 15:59:54 +0800

    sleep

--- utils/src/main/java/com/study/utils/UtilsApplication.java
+++ utils/src/main/java/com/study/utils/UtilsApplication.java
@@ -10,6 +10,17 @@ public class UtilsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UtilsApplication.class, args);
+
+/*        int i = 0;
+        while (i < 100) {
+            i++;
+            try {
+                System.out.println("sleep  " + i);
+                Thread.sleep(1_000_000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }*/
     }
 
 }
