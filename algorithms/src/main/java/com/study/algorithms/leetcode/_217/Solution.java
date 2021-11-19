831a8a2487aa6f044a796b147bf925d290ce4698
wlm2012 <541253785@qq.com>
2021-11-19 14:03:46 +0800

    leetcode

---
+++ algorithms/src/main/java/com/study/algorithms/leetcode/_217/Solution.java
@@ -0,0 +1,16 @@
+package com.study.algorithms.leetcode._217;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>(nums.length);
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ algorithms/src/main/java/com/study/algorithms/leetcode/_53/Solution.java
@@ -0,0 +1,13 @@
+package com.study.algorithms.leetcode._53;
+
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        int pre = 0;
+        int max = nums[0];
+        for (int num : nums) {
+            pre = Math.max(pre + num, num);
+            max = Math.max(pre, max);
+        }
+        return max;
+    }
+}
