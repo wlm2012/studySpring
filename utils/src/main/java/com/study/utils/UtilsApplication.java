7ebe886133edfebec4a467aeb812ce03bf16be76
wlm2012 <541253785@qq.com>
2021-11-19 16:12:47 +0800

    main

--- utils/src/main/java/com/study/utils/UtilsApplication.java
+++ utils/src/main/java/com/study/utils/UtilsApplication.java
@@ -3,13 +3,22 @@ package com.study.utils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 @EnableScheduling
 @SpringBootApplication
 public class UtilsApplication {
 
+//    @Resource
+//    private static RestTemplate okhttpRestTemplate;
+
     public static void main(String[] args) {
         SpringApplication.run(UtilsApplication.class, args);
+
+//        String forObject = okhttpRestTemplate.getForObject("https://www.baidu.com", String.class);
+//        System.out.println(forObject);
 
 /*        int i = 0;
         while (i < 100) {
