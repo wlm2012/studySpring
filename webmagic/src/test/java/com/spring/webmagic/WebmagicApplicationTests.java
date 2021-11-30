0a32e59050bb8740a3c02d6dd1b35af823ff4c8b
wlm2012 <541253785@qq.com>
2021-11-30 21:31:40 +0800

    WebMagic

---
+++ webmagic/src/test/java/com/spring/webmagic/WebmagicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.webmagic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebmagicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
