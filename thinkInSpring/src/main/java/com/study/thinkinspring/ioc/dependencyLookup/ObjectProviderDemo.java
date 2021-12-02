e298314376befac1b51cfc5f0bc4a04faa12d675
wlm2012 <541253785@qq.com>
2021-12-02 15:19:40 +0800

    ObjectProviderDemo

--- thinkInSpring/src/main/java/com/study/thinkinspring/ioc/dependencyLookup/ObjectProviderDemo.java
+++ thinkInSpring/src/main/java/com/study/thinkinspring/ioc/dependencyLookup/ObjectProviderDemo.java
@@ -1,8 +1,10 @@
 package com.study.thinkinspring.ioc.dependencyLookup;
 
+import com.study.thinkinspring.ioc.overview.domain.User;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 public class ObjectProviderDemo {
 
@@ -10,13 +12,35 @@ public class ObjectProviderDemo {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
         applicationContext.register(ObjectProviderDemo.class);
         applicationContext.refresh();
+        // 依赖查找集合对象
         lookupByObjectProvider(applicationContext);
+        lookupIfAvailable(applicationContext);
+        lookupByStreamOps(applicationContext);
         applicationContext.close();
     }
 
+    private static void lookupByStreamOps(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<String> beanProvider = applicationContext.getBeanProvider(String.class);
+        beanProvider.forEach(System.out::println);
+
+    }
+
+    private static void lookupIfAvailable(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<User> beanProvider = applicationContext.getBeanProvider(User.class);
+        User user = beanProvider.getIfAvailable(User::createUser);
+        System.out.println("当前 User 对象：" + user);
+    }
+
     @Bean
+    @Primary
     public String helloWorld() {
         return "hello,World";
+    }
+
+
+    @Bean
+    public String message() {
+        return "Message";
     }
 
 
