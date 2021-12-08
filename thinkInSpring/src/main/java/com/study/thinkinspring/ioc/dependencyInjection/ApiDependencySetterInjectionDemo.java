c029fe23567f0403701c8cf6df586332fdaf1aba
wlm2012 <541253785@qq.com>
2021-12-08 22:00:02 +0800

    AnnotationDependencySetterInjectionDemo

---
+++ thinkInSpring/src/main/java/com/study/thinkinspring/ioc/dependencyInjection/ApiDependencySetterInjectionDemo.java
@@ -0,0 +1,31 @@
+package com.study.thinkinspring.ioc.dependencyInjection;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApiDependencySetterInjectionDemo {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        // 生成 UserHolder 的 BeanDefinition
+        BeanDefinition userHolderBeanDefinition = createUserHolderBeanDefinition();
+        // 注册 UserHolder 的 BeanDefinition
+        applicationContext.registerBeanDefinition("userHolder", userHolderBeanDefinition);
+        XmlBeanDefinitionReader xmlBeanDefinitionReader = new XmlBeanDefinitionReader(applicationContext);
+        String xmlResourcePath = "classpath:/META-INF/dependency-lookup-context.xml";
+        // 加载 XML 资源，解析并且生成 BeanDefinition
+        xmlBeanDefinitionReader.loadBeanDefinitions(xmlResourcePath);
+        applicationContext.refresh();
+        UserHolder userHolder = applicationContext.getBean(UserHolder.class);
+        System.out.println(userHolder);
+        applicationContext.close();
+    }
+
+    private static BeanDefinition createUserHolderBeanDefinition() {
+        BeanDefinitionBuilder definitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(UserHolder.class);
+        definitionBuilder.addPropertyReference("user", "superUser");
+        return definitionBuilder.getBeanDefinition();
+    }
+}
