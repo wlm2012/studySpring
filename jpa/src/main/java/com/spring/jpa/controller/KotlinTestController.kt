e092a360d01f181d4b31be3eb94bd80aea9ad50c
wlm2012 <541253785@qq.com>
2022-01-30 21:34:13 +0800

    kotlin

--- jpa/src/main/java/com/spring/jpa/controller/KotlinTestController.kt
+++ jpa/src/main/java/com/spring/jpa/controller/KotlinTestController.kt
@@ -5,16 +5,10 @@ import org.springframework.web.bind.annotation.RestController
 
 
 fun main() {
-    val a = 1
-    val b = 2
-    println("kotlin")
-    println("$a + $b = ${a + b}")
-    val kotlinTestController = KotlinTestController()
-    println(kotlinTestController.test())
-    println("sum = ${sum(23, 32)}")
+
 }
 
-fun sum(a: Int, b: Int) = a + b
+
 
 @RestController
 class KotlinTestController {
--- utils/src/test/java/com/study/utils/kotlin/MainKotlin.kt
+++ utils/src/test/java/com/study/utils/kotlin/MainKotlin.kt
@@ -3,12 +3,12 @@ package com.study.utils.kotlin
 import org.junit.jupiter.api.Test
 
 
-fun main() {
-}
-
 class MainKotlin(var height: Double, length: Double) {
     var result = height + length
     var time = height * length
+
+
+//    constructor() : this(0.0, 0.0)
 
     @Test
     fun test() {
@@ -18,7 +18,7 @@ class MainKotlin(var height: Double, length: Double) {
         println("mainKotlin.height = ${mainKotlin.height}")
     }
 
-    @Test
+/*    @Test
     fun test1() {
         var a = 1
         val s1 = "a is $a"
@@ -26,9 +26,9 @@ class MainKotlin(var height: Double, length: Double) {
         // a was 1,but now is 2
         val s2 = "${s1.replace("is", "was")},but now is $a"
         println(s2)
-    }
+    }*/
 
-    @Test
+/*    @Test
     fun test2() {
         val items = listOf("apple", "banana", "kiwifruit")
         for (index in items.indices) {
@@ -38,5 +38,19 @@ class MainKotlin(var height: Double, length: Double) {
         for (x in 1..3) {
             println(x)
         }
-    }
+    }*/
+
+//    private fun sum(a: Int, b: Int) = a + b
+
+/*    @Test
+    fun test3() {
+        val a = 1
+        val b = 2
+        println("kotlin")
+        println("$a + $b = ${a + b}")
+        println("sum = ${sum(23, 32)}")
+        println("sum = ${sum(32, 132)}")
+    }*/
+
+
 }
