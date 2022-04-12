be95bc5b0fc0e4db62fa1fddc840b48946c0186e
wlm2012 <541253785@qq.com>
2022-04-12 21:23:47 +0800

    leetcode

--- algorithms/src/main/java/com/study/algorithms/leetcode/_14/Solution.java
+++ algorithms/src/main/java/com/study/algorithms/leetcode/_14/Solution.java
@@ -2,6 +2,23 @@ package com.study.algorithms.leetcode._14;
 
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        return "";
+        char[] chars = strs[0].toCharArray();
+        int num = strs[0].length();
+        for (int i = 1; i < strs.length; i++) {
+            if (num == 0) {
+                return "";
+            }
+
+            char[] chars1 = strs[i].toCharArray();
+            num=Math.min(num,strs[i].length());
+            for (int j = 0; j < num; j++) {
+                if (chars[j] != chars1[j]) {
+                    num = j;
+                    break;
+                }
+            }
+
+        }
+        return String.valueOf(chars, 0, num);
     }
 }
