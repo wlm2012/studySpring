9728be70e470bc3c8fdbf1c17563480562496883
wlm2012 <541253785@qq.com>
2022-04-20 23:00:59 +0800

    leetcode

--- algorithms/src/main/java/com/study/algorithms/leetcode/_821/Solution.java
+++ algorithms/src/main/java/com/study/algorithms/leetcode/_821/Solution.java
@@ -6,18 +6,18 @@ class Solution {
     public int[] shortestToChar(String s, char c) {
         char[] chars = s.toCharArray();
         ArrayList<Integer> list = new ArrayList<>();
-        list.add(Integer.MIN_VALUE);
+        list.add(-10001);
         for (int i = 0; i < chars.length; i++) {
             if (chars[i] == c) {
                 list.add(i);
             }
         }
-        list.add(Integer.MAX_VALUE);
+        list.add(10001);
         int[] ints = new int[s.length()];
-        int j = 1;
+        int j = 0;
         for (int i = 0; i < ints.length; i++) {
             ints[i] = Math.min(Math.abs(list.get(j) - i), Math.abs(list.get(j + 1) - i));
-            if (i == list.get(j)) {
+            if (i == list.get(j + 1)) {
                 j++;
             }
         }
--- algorithms/src/test/java/com/study/algorithms/leetcode/_821/SolutionTest.java
+++ algorithms/src/test/java/com/study/algorithms/leetcode/_821/SolutionTest.java
@@ -8,11 +8,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
 
-    //todo [3,2,1,0,1,0,0,1,2,2,1,0]
+    //[3,2,1,0,1,0,0,1,2,2,1,0]
     @Test
     void test() {
         Solution solution = new Solution();
         System.out.println(Arrays.toString(solution.shortestToChar("loveleetcode", 'e')));
+    }
+
+    //[2, 1, 0, 1]
+    @Test
+    void test2() {
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.shortestToChar("aaba", 'b')));
+    }
+
+    @Test
+    void test1(){
+        System.out.println(Math.abs(-1));
     }
 
 }
