be0fa05b1e57c9be2ec36e75707bf008a1f8cd85
wlm2012 <541253785@qq.com>
2022-05-05 21:30:51 +0800

    leetcode

--- algorithms/src/main/java/com/study/algorithms/leetcode/_417/Solution.java
+++ algorithms/src/main/java/com/study/algorithms/leetcode/_417/Solution.java
@@ -1,7 +1,10 @@
 package com.study.algorithms.leetcode._417;
 
+import java.util.LinkedList;
+import java.util.List;
+
 class Solution {
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-
+        return new LinkedList<>();
     }
 }
