72f5b93eb1161c446eb8adf2f1c45347a8815b7e
wlm2012 <541253785@qq.com>
2022-07-22 17:36:50 +0800

    json

--- mybatis-plus/src/main/java/com/study/mybatisplus/domain/entity/TJson.java
+++ mybatis-plus/src/main/java/com/study/mybatisplus/domain/entity/TJson.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.handlers.JacksonTypeHandler;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import lombok.Data;
@@ -19,6 +20,7 @@ public class TJson {
 
     private Integer orderId;
 
-    @TableField(typeHandler = JacksonTypeHandler.class)
-    private JsonArray content;
+//    @TableField(typeHandler = JacksonTypeHandler.class)
+//    @JsonFormat
+    private String content;
 }
