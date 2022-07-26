587b7afb378ce2a88d43cb1686f6d04a62ef53d1
wlm2012 <541253785@qq.com>
2022-07-26 20:21:00 +0800

    websocket

--- jpa/src/main/java/com/spring/jpa/controller/WebSocketController.java
+++ jpa/src/main/java/com/spring/jpa/controller/WebSocketController.java
@@ -7,7 +7,6 @@ import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDateTime;
 
