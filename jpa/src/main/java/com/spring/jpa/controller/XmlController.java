af6e4c24d6b0512b8c465cab0a5468503ad41111
wlm2012 <541253785@qq.com>
2022-08-30 17:19:41 +0800

    xml

---
+++ jpa/src/main/java/com/spring/jpa/controller/XmlController.java
@@ -0,0 +1,11 @@
+package com.spring.jpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class XmlController {
+
+
+}
