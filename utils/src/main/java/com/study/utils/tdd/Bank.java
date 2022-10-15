636a733e3fb681f262ecd7ee514b7ef2daca004d
wlm2012 <541253785@qq.com>
2022-10-15 15:15:12 +0800

    tdd 13

--- utils/src/main/java/com/study/utils/tdd/Bank.java
+++ utils/src/main/java/com/study/utils/tdd/Bank.java
@@ -2,11 +2,6 @@ package com.study.utils.tdd;
 
 public class Bank {
     public Money reduce(Expression source, String to) {
-        if (source instanceof Money money) {
-            return money.reduce(to);
-        }
-
-        Sum sum = (Sum) source;
-        return sum.reduce(to);
+        return source.reduce(to);
     }
 }
