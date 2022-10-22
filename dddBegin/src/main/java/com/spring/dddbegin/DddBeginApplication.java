97ebfb7376a88371a39ac0a632841d0305009d17
wlm2012 <541253785@qq.com>
2022-10-22 11:35:15 +0800

    ddd

---
+++ dddBegin/src/main/java/com/spring/dddbegin/DddBeginApplication.java
@@ -0,0 +1,13 @@
+package com.spring.dddbegin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddBeginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddBeginApplication.class, args);
+	}
+
+}
---
+++ dddBegin/src/test/java/com/spring/dddbegin/DddBeginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.dddbegin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddBeginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
