7e88f79969aa25c75feb1d52bff85a2e28481c2b
wlm2012 <541253785@qq.com>
2022-11-03 23:07:27 +0800

    82. 删除排序链表中的重复元素 II

---
+++ algorithms/src/main/java/com/study/algorithms/leetcode/_82/Solution.java
@@ -0,0 +1,22 @@
+package com.study.algorithms.leetcode._82;
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummy = new ListNode(0, head);
+        ListNode cur = dummy;
+        while (cur.next != null && cur.next.next != null) {
+            if (cur.next.val == cur.next.next.val) {
+                int val = cur.next.val;
+                while (cur.next != null && val == cur.next.val) {
+                    cur.next = cur.next.next;
+                }
+            } else {
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
+    }
+}
