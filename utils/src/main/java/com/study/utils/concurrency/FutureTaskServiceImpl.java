e6b15a38dc07e3ae216d71ca351c7e8b74b760ee
wlm2012 <541253785@qq.com>
2022-11-03 20:09:07 +0800

    FutureTaskServiceImplTest

---
+++ utils/src/main/java/com/study/utils/concurrency/FutureTaskServiceImpl.java
@@ -0,0 +1,11 @@
+package com.study.utils.concurrency;
+
+import com.study.utils.concurrency.asynProgramming._2.SyncExample;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class FutureTaskServiceImpl {
+
+
+}
---
+++ utils/src/test/java/com/study/utils/concurrency/FutureTaskServiceImplTest.java
@@ -0,0 +1,63 @@
+package com.study.utils.concurrency;
+
+import com.study.utils.concurrency.asynProgramming._2.SyncExample;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FutureTaskServiceImplTest {
+
+
+    private final int AVALIABLE_PROCESSORS = Runtime.getRuntime().availableProcessors();
+    private final ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(AVALIABLE_PROCESSORS,
+            AVALIABLE_PROCESSORS * 2, 1,
+            TimeUnit.MINUTES, new LinkedBlockingQueue<>(5), new ThreadPoolExecutor.CallerRunsPolicy());
+
+    @Test
+    void asyncFutureTask() throws ExecutionException, InterruptedException {
+        long start = System.currentTimeMillis();
+        FutureTask<String> futureTask = new FutureTask<>(SyncExample::returnA);
+        Thread thread = new Thread(futureTask, "threadA");
+        thread.start();
+
+        String returnB = SyncExample.returnB();
+        System.out.println(System.currentTimeMillis() - start);
+        System.out.println("returnB = " + returnB);
+
+        String taskAResult = futureTask.get();
+        System.out.println(System.currentTimeMillis() - start);
+        System.out.println("taskAResult = " + taskAResult);
+    }
+
+    @Test
+    void asyncPool() throws ExecutionException, InterruptedException {
+        long start = System.currentTimeMillis();
+        FutureTask<String> futureTask = new FutureTask<>(SyncExample::returnA);
+        threadPoolExecutor.execute(futureTask);
+
+        String returnB = SyncExample.returnB();
+        System.out.println(System.currentTimeMillis() - start);
+        System.out.println("returnB = " + returnB);
+
+        String taskAResult = futureTask.get();
+        System.out.println(System.currentTimeMillis() - start);
+        System.out.println("taskAResult = " + taskAResult);
+    }
+
+    @Test
+    void asyncPool1() throws ExecutionException, InterruptedException {
+        long start = System.currentTimeMillis();
+        Future<String> future = threadPoolExecutor.submit(SyncExample::returnA);
+
+        String returnB = SyncExample.returnB();
+        System.out.println(System.currentTimeMillis() - start);
+        System.out.println("returnB = " + returnB);
+
+        String taskAResult = future.get();
+        System.out.println(System.currentTimeMillis() - start);
+        System.out.println("taskAResult = " + taskAResult);
+    }
+
+}
